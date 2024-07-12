8637a55326a74d63ceec121d615ecec1c7dd7e89
BruhaBruh <drugsho.jaker@gmail.com>
2024-07-12 18:34:05 +0500

    ci: fix build and action

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import react from '@vitejs/plugin-react';
 import autoprefixer from 'autoprefixer';
 import { sync } from 'glob';
 import path from 'node:path';
-import url from 'node:url';
+import preserveDirectives from 'rollup-plugin-preserve-directives';
 import tailwindcss from 'tailwindcss';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
@@ -11,10 +11,11 @@ export default defineConfig({
   plugins: [
     react(),
     dts({
-      tsconfigPath: './tsconfig.json',
       outDir: 'dist',
+      tsconfigPath: './tsconfig.json',
       insertTypesEntry: true,
     }),
+    preserveDirectives(),
   ],
   resolve: {
     alias: {
@@ -27,31 +28,49 @@ export default defineConfig({
     },
   },
   build: {
-    lib: {
-      entry: 'src/index.ts',
-      name: 'PostnowUI',
-      formats: ['es'],
-    },
+    // lib: {
+    //   entry: 'src/index.ts',
+    //   name: 'PostnowUI',
+    //   formats: ['es'],
+    // },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: [
+        'react',
+        'react-dom',
+        'react/jsx-runtime',
+        '@internationalized/date',
+        '@radix-ui/react-slot',
+        'class-variance-authority',
+        'clsx',
+        'react-aria',
+        'react-stately',
+        'tailwind-merge',
+      ],
+      preserveEntrySignatures: 'exports-only',
       output: {
+        // globals: {
+        //   react: 'React',
+        //   'react-dom': 'ReactDOM',
+        // },
+        dir: 'dist',
+        format: 'es',
         entryFileNames: '[name].js',
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
+        chunkFileNames: '[name].js',
+        preserveModules: true,
+        preserveModulesRoot: 'src',
       },
-      input: Object.fromEntries(
-        sync('src/**/*.{ts,tsx}', {
-          ignore: ['src/**/*.stories.{ts,tsx}', 'src/stories/**'],
-        }).map((file) => [
-          path.relative(
-            'src',
-            file.slice(0, file.length - path.extname(file).length),
-          ),
-          url.fileURLToPath(new URL(file, import.meta.url)),
-        ]),
-      ),
+      input: sync('src/**/*.{ts,tsx}', {
+        ignore: [
+          'src/**/*.{stories,types}.{ts,tsx}',
+          'src/**/*.d.ts',
+          'src/stories/**',
+        ],
+      }).reduce((entries, file) => {
+        const entry = path.relative('src', file).replace(/\.[^/.]+$/, '');
+        const newEntries = entries;
+        newEntries[entry] = path.resolve(__dirname, file);
+        return entries;
+      }, {}),
     },
   },
 });
