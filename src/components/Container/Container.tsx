2f9cb77afc75ddd2b2894d5a5bce958768d059cf
BruhaBruh <drugsho.jaker@gmail.com>
2024-07-15 13:04:19 +0500

    feat: improve container

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -5,10 +5,7 @@ import { cn } from '@/utility/cn';
 import { Slot } from '@radix-ui/react-slot';
 import React from 'react';
 import { ContainerProps } from './Container.types';
-import {
-  containerVariants,
-  innerContainerVariants,
-} from './Container.variants';
+import { containerVariants } from './Container.variants';
 
 export const Container = React.forwardRef<HTMLElement, ContainerProps>(
   ({ color, className, asChild, children, ...props }, forwardedRef) => {
@@ -22,7 +19,7 @@ export const Container = React.forwardRef<HTMLElement, ContainerProps>(
         ref={ref}
         className={cn(containerVariants({ color }), className)}
       >
-        <div className={innerContainerVariants()}>{children}</div>
+        {children}
       </Comp>
     );
   },
--- src/components/Container/Container.variants.ts
+++ src/components/Container/Container.variants.ts
@@ -1,30 +1,64 @@
 import { cva } from 'class-variance-authority';
 
-export const containerVariants = cva([], {
-  variants: {
-    color: {
-      primary: ['bg-background text-foreground'],
-      secondary: ['bg-secondary text-on-secondary'],
-      brand: ['bg-brand text-on-brand'],
-      brandSubdued: ['bg-brand-subdued text-on-brand-subdued'],
-      info: ['bg-info text-on-info'],
-      infoSubdued: ['bg-info-subdued text-on-info-subdued'],
-      success: ['bg-success text-on-success'],
-      successSubdued: ['bg-success-subdued text-on-success-subdued'],
-      caution: ['bg-caution text-on-caution'],
-      cautionSubdued: ['bg-caution-subdued text-on-caution-subdued'],
-      critical: ['bg-critical text-on-critical'],
-      criticalSubdued: ['bg-critical-subdued text-on-critical-subdued'],
+export const containerVariants = cva(
+  [
+    'container',
+    'px-md py-3xl',
+    'grid gap-md',
+    'grid-cols-2 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-8 xl:grid-cols-10 2xl:grid-cols-12',
+    'relative',
+    "before:content-[''] before:absolute before:top-0 before:bottom-0 before:right-full before:w-screen",
+    "after:content-[''] after:absolute after:top-0 after:bottom-0 after:left-full after:w-screen",
+  ],
+  {
+    variants: {
+      color: {
+        primary: [
+          'bg-background text-foreground',
+          'before:bg-background after:bg-background',
+        ],
+        secondary: [
+          'bg-secondary text-on-secondary',
+          'before:bg-secondary after:bg-secondary',
+        ],
+        brand: ['bg-brand text-on-brand', 'before:bg-brand after:bg-brand'],
+        brandSubdued: [
+          'bg-brand-subdued text-on-brand-subdued',
+          'before:bg-brand-subdued after:bg-brand-subdued',
+        ],
+        info: ['bg-info text-on-info', 'before:bg-info after:bg-info'],
+        infoSubdued: [
+          'bg-info-subdued text-on-info-subdued',
+          'before:bg-info-subdued after:bg-info-subdued',
+        ],
+        success: [
+          'bg-success text-on-success',
+          'before:bg-success after:bg-success',
+        ],
+        successSubdued: [
+          'bg-success-subdued text-on-success-subdued',
+          'before:bg-success-subdued after:bg-success-subdued',
+        ],
+        caution: [
+          'bg-caution text-on-caution',
+          'before:bg-caution after:bg-caution',
+        ],
+        cautionSubdued: [
+          'bg-caution-subdued text-on-caution-subdued',
+          'before:bg-caution-subdued after:bg-caution-subdued',
+        ],
+        critical: [
+          'bg-critical text-on-critical',
+          'before:bg-critical after:bg-critical',
+        ],
+        criticalSubdued: [
+          'bg-critical-subdued text-on-critical-subdued',
+          'before:bg-critical-subdued after:bg-critical-subdued',
+        ],
+      },
+    },
+    defaultVariants: {
+      color: 'primary',
     },
   },
-  defaultVariants: {
-    color: 'primary',
-  },
-});
-
-export const innerContainerVariants = cva([
-  'container',
-  'px-md py-3xl',
-  'grid gap-md',
-  'grid-cols-2 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-8 xl:grid-cols-10 2xl:grid-cols-12',
-]);
+);
