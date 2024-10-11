a3c6f3b6e6ce6e1f7111263e1123b5a192b8859f
BruhaBruh <drugsho.jaker@gmail.com>
2024-10-11 11:34:09 +0500

    chore: updare versions in storybook and package.json

--- .storybook/theme.ts
+++ .storybook/theme.ts
@@ -6,6 +6,6 @@ export const theme = create({
     'Inter, ui-sans-serif, system-ui, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
   fontCode:
     '"JetBrains Mono", ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
-  brandTitle: 'BruhaBruh UI v1.4.2',
+  brandTitle: 'BruhaBruh UI v2.0.0',
   brandImage: undefined,
 });
