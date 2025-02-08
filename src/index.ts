9cd5eb5e48bb8da24d5aaa384e524df34b931e82
BruhaBruh <drugsho.jaker@gmail.com>
2025-02-09 02:56:11 +0500

    refactor: move to HashRouter

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,3 @@ export * from './components';
 export * from './config';
 export * from './hooks';
 export * from './types';
-
-// Name & Group
-// Layout & Positioning
-// Box Model
-// Typography
-// Background
-// Borders & Shadows
-// Effects & Filters
-// Transitions & Animation
-// States
-// Interaction States
-// Ripple Effect
-// Before & After
-// Flex & Grid
-// Tables
-// Spacing
-// Transforms
-// Filters
-// Accessibility
